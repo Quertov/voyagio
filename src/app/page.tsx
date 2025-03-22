6a2d82000ca952560b6ce662bd8b128d4ef3f734
lucas <smoooki44@gmail.com>
2025-03-22 18:16:00 +0200

    feat: banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,13 @@
 
 import styles from '@/styles/Home.module.css';
 import Search from "@/components/Search";
+import { Banner } from '@/components/Banner';
 
 export default function Home() {
   return (
     <div className={ styles.root }>
       <Search />
+      <Banner />
     </div>
   );
 }
---
+++ src/components/Banner.tsx
@@ -0,0 +1,45 @@
+'use client';
+
+import { FC, useEffect, useState } from "react";
+import { Manrope } from "next/font/google";
+import styles from '@/styles/Banner.module.css';
+import Image from "next/image";
+
+const RobotoSlab = Manrope({
+	variable: "--manrope-font",
+	weight: ['400', '500', '700'],
+  style: ['normal'],
+  subsets: ['cyrillic', 'cyrillic-ext', 'latin', 'latin-ext']
+});
+
+export const Banner: FC = () => {
+	const [currentBanner, setCurrentBanner] = useState<string>('banner1');
+
+	useEffect(() => {
+		let bannerNumber = 1;
+
+		const bannerChangeInterval = setInterval(() => {
+			if (bannerNumber > 3) {
+				bannerNumber = 1;
+			}
+			setCurrentBanner(`banner${bannerNumber}`);
+			++bannerNumber;
+		}, 10000);
+
+		return () => clearInterval(bannerChangeInterval);
+	}, []);
+
+	return (
+		<div className={ `${ styles.banner__container } ${RobotoSlab.variable} antialiased` }>
+			<div className={ styles.banner__image_container }>
+				<div className={ styles.banner__text_container }>
+					<div className={ styles.banner__text_wrapper }>
+						<h1>Сплануй свою подорож</h1>
+						<p>Відкрий для себе нові місця та незабутні враження разом із <strong className="font-bold underline underline-offset-8">Voyagio</strong></p>
+					</div>
+				</div>
+				<Image alt="" src={ `/images/${currentBanner}.webp` } width={1200} height={700} className={ styles.banner__image } />
+			</div>
+		</div>
+	)
+}
