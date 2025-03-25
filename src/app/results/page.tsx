3acd3668af732ccbc1158cfa104a9ecbea663cad
lucas <smoooki44@gmail.com>
2025-03-25 14:51:24 +0200

    feat: pagination

--- src/app/results/page.tsx
+++ src/app/results/page.tsx
@@ -1,10 +1,11 @@
 'use client';
 
-import { FC, useState, useEffect } from "react";
+import { FC, useState, useEffect, useMemo } from "react";
 import styles from '@/styles/results/Results.module.css';
 import { useSearchPlacesQuery } from "@/store/slices/apiSlice";
 import { useSearchParams } from "next/navigation";
 import { usePhotos } from "@/hooks/usePhotos";
+import Image from "next/image";
 
 // types
 import { IPlace } from "@/types/types";
@@ -14,11 +15,25 @@ import { Loader } from "@/components/utils/Loader";
 import { NoResults } from "@/components/utils/NoResults";
 import { Place } from "@/components/Place";
 
+// icons
+import ArrowLeft from '../../../public/icons/arrow_left.svg';
+import ArrowRight from '../../../public/icons/arrow_right.svg';
+
+const ITEMS_PER_PAGE = 9;
+
 const Results: FC = () => {
 	const searchParams = useSearchParams();
 	const query = searchParams.get('query') || '';
 	const category = Number(searchParams.get('category'));
 	const { data, error, isLoading } = useSearchPlacesQuery({category, query});
+
+	const [currentPage, setCurrentPage] = useState<number>(1);
+	const startIndex = useMemo(() => ( (currentPage - 1) * ITEMS_PER_PAGE ), [currentPage]);
+	const endIndex = useMemo(() => ( startIndex + ITEMS_PER_PAGE ), [startIndex]);
+	const currentItems = useMemo(() => ( data?.results.slice(startIndex, endIndex) ), [startIndex, endIndex, data?.results]);
+	// @ts-ignore
+	const totalPages = useMemo(() => ( Math.ceil(data?.results.length / ITEMS_PER_PAGE) ), [data?.results.length]);
+
 	const { fetchPhotos } = usePhotos();
 
 	const [photos, setPhotos] = useState<{ [key: string]: string[] }>({});
@@ -48,7 +63,7 @@ const Results: FC = () => {
 		<div className={ styles.root }>
 			<h1 className={ styles.results__title }>Результати за запитом: "{ query.charAt(0).toUpperCase() + query.slice(1).toLowerCase() }"</h1>
 			<section className={ styles.results__places_grid }>
-				{ data.results.map((place: IPlace, i) => (
+				{ currentItems?.map((place: IPlace, i) => (
 					<Place
 						key={ i }
 						name={ place.name }
@@ -56,6 +71,19 @@ const Results: FC = () => {
 						fsqId={ place.fsq_id } />
 				)) }
 			</section>
+			<div className={ styles.pagination__buttons_container }>
+				<button
+					disabled={ currentPage === 1 }
+					onClick={ () => setCurrentPage((prev) => --prev) }>
+						<Image alt="" src={ArrowLeft} width={24} height={24} className="ml-[8px]" />
+					</button>
+				<span>Сторінка { currentPage } із { totalPages }</span>
+				<button
+					disabled={ currentPage === totalPages }
+					onClick={ () => setCurrentPage((prev) => ++prev) }>
+						<Image alt="" src={ArrowRight} width={24} height={24} className="ml-[2px]" />
+					</button>
+			</div>
 		</div>
 	)
 };
